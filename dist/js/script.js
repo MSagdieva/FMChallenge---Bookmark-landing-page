70fa6c94b2a23845410f8b73213a9633ce646e1f
MSagdieva <grigorieva.ms@yandex.ru>
2023-05-01 23:49:51 +0300

    add main-heading, fix fonts, add feature tabs

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,3 +8,12 @@ window.addEventListener('click', function(e){
 
     }});
 ;
+const tabs = document.querySelectorAll('.tab-label');
+Array.from(tabs).map((tab, i) => {
+    tab.addEventListener('click', ()=>{
+        const activeTab = document.querySelector('.info-block.active');
+        activeTab.classList.remove('active');
+        const newActiveTab = document.getElementById(`tab-${i}`);
+        newActiveTab.classList.add('active');
+    })
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
 @@include('burgerShow.js');
+const tabs = document.querySelectorAll('.tab-label');
+Array.from(tabs).map((tab, i) => {
+    tab.addEventListener('click', ()=>{
+        const activeTab = document.querySelector('.info-block.active');
+        activeTab.classList.remove('active');
+        const newActiveTab = document.getElementById(`tab-${i}`);
+        newActiveTab.classList.add('active');
+    })
+});
